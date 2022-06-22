c093dca1e540855e5c964691cbb1f7fb7d5b42ab
Timo Weberruß <timo.weberruss@student.kit.edu>
2022-06-22 02:18:15 +0200

    Add file reading example

--- demo.py
+++ demo.py
@@ -1,10 +1,12 @@
 from box_classifier import *
 
 if __name__ == '__main__':
-    box1 = Box("tall", 1, 9, 1)
-    box2 = Box("wide", 3, 1, 3)
-    box_types = [box1, box2]
-    classifier = BoxClassifier(box_types, 0.15)
+    # box_types = [Box("tall", 1, 9, 1), Box("wide", 3, 1, 3)]
+    box_types = BoxReader(',').read_from_file('demoboxes.txt')
+
+    allowed_deviation = 0.15
+    classifier = BoxClassifier(box_types, allowed_deviation)
+
     boxes = [Box("kinda wide box", 2.9, 1.1, 3.08), Box("kinda tall box", 1.01, 8.8, 0.9), Box("weird cubic box", 5, 5, 5)]
     for box in boxes:
         result_type = classifier.classify(box)
